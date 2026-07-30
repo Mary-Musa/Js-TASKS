a9ea9f2433124e91af1f384542c0072284951fe4
Mary-Musa <kakamary1@gmail.com>
2026-07-30 20:40:33 +0300

    modified Tasks.js and Tasks.txt

--- Tasks.js
+++ Tasks.js
@@ -1,2 +1,16 @@
+// Global and local variables in JS
+
+let platform = "YouTube";// global variable
+
+function contentCreator(name){
+    let creatorName = name;//local variable
+
+    console.log("platform:",platform);
+    console.log("creator:",creatorName);
+}
+
+contentCreator("Pendo");
+contentCreator("Mary");
+
 
 
