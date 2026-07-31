dc486831492d8ed1d227a5ca8a3e7e31f70f583a
Mary-Musa <kakamary1@gmail.com>
2026-08-01 00:38:27 +0300

    Task2 - closures

--- Tasks.js
+++ Tasks.js
@@ -1,13 +1,42 @@
-//Task1-global and local scope
+//Task1-global and local scope in Js
 
 let platform = "Youtube";
 
-function contentCreator(){
+function contentCreator(name){
 
-    let creatorName = "Pendo";
+    let creatorName = name;
 
     console.log("Platform:",platform);
     console.log("Creator:",creatorName);
 }
 
-contentCreator();
+contentCreator("Pendo");
+contentCreator("Mary");
+
+
+
+//Task 11- counter closure/closures in Js
+
+function createCounter(){
+
+    let count = 0;
+
+    return{
+
+        increment(){
+            count++;
+            return count;
+        }
+        
+    }
+}
+
+const counter = createCounter();
+
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.increment());
+
+
+
+
